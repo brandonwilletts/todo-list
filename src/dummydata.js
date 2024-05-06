032ce1281189ed9046d69d02191f31ed7f10b954
Brandon Willetts”git config --global user.email “brandonwilletts@gmail.com”git config --global user.name Brandon <brandonwilletts@gmail.com>
2024-05-05 19:54:50 -0700

    Create dummy data for tasks and render

--- src/dummydata.js
+++ src/dummydata.js
@@ -6,19 +6,41 @@ export function createDummyData() {
     const projectSchool = createProject("School");
     const projectPersonal = createProject("Personal");
     const projectOther = createProject("Other");
+
+    const date = new Date();
     
     const taskOne = createTask(
-        "Title 1",
-        "Description",
-        "Due Date",
-        "Priority",
-        "Project"
+        "Finish TPS report",
+        "Submit to both Bobs",
+        date,
+        "High",
+        projectWork
     );
     const taskTwo = createTask(
-        "Title 2",
-        "Description",
-        "Due Date",
-        "Priority",
-        "Project"
+        "Study for mid-term",
+        "Make sure to review Chapters 4 and 5",
+        date,
+        "Medium",
+        projectSchool
+    );
+    const taskThree = createTask(
+        "Buy groceries",
+        "Milk, bread, cheese, eggs",
+        date,
+        "Low",
+        projectPersonal
+    );
+    const taskFour = createTask(
+        "Get car serviced",
+        "Fairview Auto",
+        date,
+        "Medium",
+        projectOther
+    );
+    const taskFive = createTask(
+        "See if Inception is playing",
+        "Invite Steve",
+        date,
+        "Low"
     );
 };
--- src/tasks.js
+++ src/tasks.js
@@ -1,9 +1,9 @@
 import { createElement } from "./elements";
 
 class Task {
-    constructor(title, description, dueDate, priority, project) {
+    constructor(title, notes, dueDate, priority, project) {
         this.title = title;
-        this.description = description;
+        this.notes = notes;
         this.dueDate = dueDate;
         this.priority = priority;
         this.project = project;
@@ -12,8 +12,8 @@ class Task {
 
 let tasks = [];
 
-export function createTask(title, description, dueDate, priority, project) {
-    const task = new Task (title, description, dueDate, priority, project);
+export function createTask(title, notes, dueDate, priority, project) {
+    const task = new Task (title, notes, dueDate, priority, project);
     tasks.push(task);
     return task
 }
@@ -22,7 +22,7 @@ export const getTasks = (function() {
     const all = () => tasks;
     const today = () => tasks;
     const overdue = () => tasks;
-    const project = (projectName) => tasks;
+    const project = (project) => tasks;
     return { all, today, overdue, project };
 })();
 
